214147bbdbcc1ae0d0ae5b72059d2eda081bdb07
jerryh91 <jerryhou91@gmail.com>
2018-01-28 13:19:05 -0600

    Added inputs to add artist and associated albums

--- src/app/artists/artists.component.ts
+++ src/app/artists/artists.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { artistAlbumsHashMap } from '../commons/commons';
 
 @Component({
   selector: 'app-artists',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistsComponent implements OnInit {
 
+
+  artistsList: artistAlbumsHashMap = {};
+  artist: Object;
+
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  addNewArtistAndAlbum(inputArtist: string, inputArtistAlbum?: string) {
+    if (this.artistsList[inputArtist] == null) {
+      this.artistsList[inputArtist] = new Set();
+    }
+    this.artistsList[inputArtist].add(inputArtistAlbum);
   }
 
 }
